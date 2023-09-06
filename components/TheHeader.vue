7685399152f8630b91c62a57e0e8d866384f27e2
Juris-Berkulis <krztfjrbhm1@gmail.com>
2023-09-06 13:38:38 +0300

    1. Add header.

---
+++ components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<header class="header">
+    <IconsLogo />
+</header>
+</template>
+
+<style scoped lang="scss">
+.header {
+    height: 85px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+    <TheHeader />
     <main class="main">
         <slot />
     </main>
